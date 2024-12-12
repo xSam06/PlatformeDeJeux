5ee12bfcf015528bd95cda8fc019929090e72320
xSam06 <samy.bouamar@univ-rouen.fr>
2024-12-12 13:18:03 +0100

    jeu morpion

--- Controllers/Hubs/GameHub.cs
+++ Controllers/Hubs/GameHub.cs
@@ -2,9 +2,22 @@ using Microsoft.AspNetCore.SignalR;
 
 public class GameHub : Hub
 {
-    public async Task SendMove(string player, int row, int col)
+
+    //rejoindre le lobby
+   public async Task JoinGame(string gameId)
     {
-        // Notifie tous les clients du mouvement
-        await Clients.Others.SendAsync("ReceiveMove", player, row, col);
+        await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+        await Clients.Group(gameId).SendAsync("PlayerJoined", Context.ConnectionId);
+    }
+
+    
+    public async Task MakeMove(string gameId, int x, int y, string player)
+    {
+        await Clients.Group(gameId).SendAsync("MoveMade", x, y, player);
+    }
+
+    public async Task SendMessage(string gameId, string message)
+    {
+        await Clients.Group(gameId).SendAsync("ReceiveMessage", message);
     }
 }
